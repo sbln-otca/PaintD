35e273c2968101470f22bb3a2879f9a4490d2ed7
Я <@DESKTOP-DQ4HCE0>
2025-11-01 10:58:39 +1000

    Добавьте файлы проекта.

---
+++ PaintD/MainWindow.axaml.cs
@@ -0,0 +1,15 @@
+using Avalonia.Controls;
+
+namespace PaintD
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Red(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+        }
+    }
+}
